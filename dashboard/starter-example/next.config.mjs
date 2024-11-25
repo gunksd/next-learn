d48a840a5593156cee0b9c3e9480aae76d7bb5cb
gunksd <3056571032@qq.com>
2024-11-25 22:56:53 +0800

    0.4

--- dashboard/starter-example/next.config.mjs
+++ dashboard/starter-example/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
- 
+
 const nextConfig = {
   experimental: {
-    ppr: 'incremental',
+    ppr: true, // 设置为 true 或 false 以启用或禁用
   },
 };
- 
+
 export default nextConfig;
